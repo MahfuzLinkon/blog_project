93724c58d0b1d3936362e178bd8bfe189480d54e
mahfuzlinkon <mahfuzur076@gmail.com>
2022-12-09 02:07:12 +0600

    Jetstream Install

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontendController;
 
 Route::get('/', [FrontendController::class, 'home'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
